65030129452da20ca7e48dc41a5c1546e6c59602
Daniil SkyAimer <bezuglich7@gmail.com>
2023-01-08 21:22:38 +0400

    add MyCreateComponents with h and empty button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 
+function MyCreateComponents() {
+  return (
+    <div>
+      <h2>Заголовок Reusable Components</h2>
+      <button> click </button>
+    </div>
+  );
+}
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <MyCreateComponents />
+      <MyCreateComponents />
+      <MyCreateComponents />
+    </div>
+  );
 }
 
 export default App;
