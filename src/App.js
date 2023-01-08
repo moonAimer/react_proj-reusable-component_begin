c1a80750e3ccd753e1f1e011a4b19864d3494142
Daniil SkyAimer <bezuglich7@gmail.com>
2023-01-08 21:55:08 +0400

    add one else component with tag p and text

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import "./App.css";
 
 import MyCreateComponent from "./components/MyCreateComponent";
 
+import OneElseComponent from "./components/OneElseComponent";
+
 function App() {
   return (
     <div className="App">
       <MyCreateComponent />
+      <OneElseComponent />
       <MyCreateComponent />
+      <OneElseComponent />
       <MyCreateComponent />
     </div>
   );
---
+++ src/components/OneElseComponent.js
@@ -0,0 +1,5 @@
+function OneElseComponent() {
+  return <p>Летом тепло, зимой холодно</p>;
+}
+
+export default OneElseComponent;
