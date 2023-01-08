4d21b7da75be54387dc68e5ddfc9b69268fea1b7
Daniil SkyAimer <bezuglich7@gmail.com>
2023-01-08 21:58:29 +0400

    with div in 2 component

--- src/components/OneElseComponent.js
+++ src/components/OneElseComponent.js
@@ -1,5 +1,9 @@
 function OneElseComponent() {
-  return <p>Летом тепло, зимой холодно</p>;
+  return (
+    <div>
+      <p>Летом тепло, зимой холодно</p>
+    </div>
+  );
 }
 
 export default OneElseComponent;
